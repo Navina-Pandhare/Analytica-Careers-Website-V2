c94ab825bc55f4be82fc208a86f10379cdc6b506
Navina-Pandhare <136430200+Navina-Pandhare@users.noreply.github.com>
2024-03-13 18:06:47 +0000

    Store Application in database

--- database.py
+++ database.py
@@ -24,6 +24,25 @@ def load_job_from_db(id):
     else:
       return dict(rows[0])
 
+def add_application_to_db(job_id, application):
+  application = dict(application)
+  print(type(application), application, application["full_name"], job_id)
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+    conn.execute(
+      query, {
+        "job_id": job_id,
+        "full_name": application["full_name"],
+        "email": application["email"],
+        "linkedin_url": application["linkedin_url"],
+        "education": application["education"],
+        "work_experience": application["work_experience"],
+        "resume_url": application["resume_url"]
+      })
+    conn.commit()
+
 
   
 
